321d54076b1e6afe9bbb3df6d246de82f16cc9cd
user <user@FC09>
2016-10-21 10:04:16 +0800

    add for

--- src/fcu/iecs/oops/tiida/NissanTiida.java
+++ src/fcu/iecs/oops/tiida/NissanTiida.java
@@ -5,8 +5,14 @@ public class NissanTiida {
 	
 	public void tiida(int args){
 		
+		int number=0,i=0;
+	
+		for(i=0;i<number;i++){
+			for(int j=0;j<number;j++){
+				System.out.println("*");
+			}
 		System.out.println("*");
 		}
 		
-	
+	}
 }
